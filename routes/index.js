539da6192b7ae64ba66c39fcd69b48f1ec59dd36
Fletcher243 <mumonrift@gmail.com>
2017-11-03 00:58:55 +0000

    somethign

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+var https = require('https');
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('mtg.html', {root: 'public'});
+});
+
+router.get('/getcard', function(req,res,next){
+  var url = 'https://api.magicthegathering.io/v1/cards?name=' + req.query.q
+  request(url).pipe(res);
+});
+
+module.exports = router;
